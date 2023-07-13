d1ac702288ae5aa309c93e0f1b6a990b312ba747
Vama Mehta <vama.mehta@inqubit.in>
2023-07-13 17:32:34 +0530

    feat- MagicBus Employee Form

---
+++ magicbus_erpnext/magicbus_erpnext/doctype/mb_employee/mb_employee.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2023, Vaama and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('MB Employee', {
+	// refresh: function(frm) {
+
+	// }
+});
---
+++ magicbus_erpnext/magicbus_erpnext/doctype/mb_employee/test_mb_employee.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2023, Vaama and Contributors
+# See license.txt
+
+# import frappe
+import unittest
+
+class TestMBEmployee(unittest.TestCase):
+	pass
